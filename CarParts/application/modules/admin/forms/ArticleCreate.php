fff6eb3418ac7cb0318aa6fd9bd07c45f84d64db
Aleksis <Aleksis@Main-PC>
2013-05-03 19:50:11 +0300

    pievienota Valoda rakstiem

--- CarParts/application/modules/admin/forms/ArticleCreate.php
+++ CarParts/application/modules/admin/forms/ArticleCreate.php
@@ -3,7 +3,7 @@ class Admin_Form_ArticleCreate extends Zend_Form {
 	public function init() {
 	    $language = new Zend_Form_Element_Select('language');
 	    $language->setLabel('language')
-	             ->setMultiOptions(array('lv' => 'Latvian', 'ru' => 'Russian', 'en' => 'English', 'no' => 'Norvegian', 'fi' => 'Finnish', 'sv' => 'Swedish', 'da' => 'Danish'));
+	             ->setMultiOptions(array('lv' => 'Latvian', 'ru' => 'Russian', 'en' => 'English', 'no' => 'Norvegian', 'fi' => 'Finnish', 'sv' => 'Swedish', 'da' => 'Danish', 'nl' => 'Dutch'));
 	    
 		$name = new Zend_Form_Element_Text('name');
 		$name->setLabel('Article name');
